5b2ea4c6fe513bc3b55d8684fb421fb39ccdbdcb
devinrankin <devinrankin@proton.me>
2025-12-14 08:37:00 -0500

    Some basic utility functions to start.

---
+++ src/utils/files.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_BYTE_READING_H
+#include <vector>
+#include "types.h"
+
+std::vector<u8> read_bytes(const char* path);
+
+#endif
---
+++ src/utils/types.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_TYPES_H
+#define UTILS_TYPES_H
+#include <cstdint>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+
+#endif
